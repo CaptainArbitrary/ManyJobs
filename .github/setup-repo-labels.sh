e276fbf519946a55f763f361f0ba31bb923ab706
Jeffery Harrell <jefferyharrell@gmail.com>
2023-08-07 06:32:37 -0700

    Delete setup-repo-labels.sh script and associated labels.

--- .github/setup-repo-labels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Pull request type labels
-
-gh label create "chore" -d "A change that doesn't affect production code" -c "#c5def5" -f
-gh label create "bug" -d "Something isn't working" -c "#d73a4a" -f
-gh label create "enhancement" -d "Improvement to an existing feature" -c "#a2eeef" -f
-gh label create "feature" -d "New feature" -c "#a2eeef" -f
