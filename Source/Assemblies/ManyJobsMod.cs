9eb403e358dd350f0443aa1082456aec84fb6434
Jeffery Harrell <jefferyharrell@gmail.com>
2023-08-11 20:39:52 -0700

    Most naïve, most brute-force implementation imaginable
    - vast quantities of redundant code
    - virtually unmaintainable
    - only implements mod settings for Core, no DLCs or mods

---
+++ Source/Assemblies/ManyJobsMod.cs
@@ -0,0 +1,308 @@
+﻿using System.Globalization;
+using UnityEngine;
+using Verse;
+
+namespace ManyJobs
+{
+    public class ManyJobsMod : Verse.Mod
+    {
+        ManyJobsModSettings settings;
+        TextInfo textInfo;
+
+        float listingRectCurrentHeight;
+        Vector2 scrollPositionVector;
+
+        public ManyJobsMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<ManyJobsModSettings>();
+            textInfo = new CultureInfo("en-US", false).TextInfo;
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Many Jobs";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Rect scrollViewRect = new Rect(inRect.x, inRect.y, inRect.width - 24f, listingRectCurrentHeight);
+            Widgets.BeginScrollView(inRect, ref scrollPositionVector, scrollViewRect);
+            Listing_Standard listing = new Listing_Standard();
+            Rect listingRect = new Rect(scrollViewRect.x, scrollViewRect.y, scrollViewRect.width, 99999f);
+            listing.Begin(listingRect);
+
+            Color savedColor = GUI.color;
+            GUI.color = ColorLibrary.RedReadable;
+            Text.Anchor = TextAnchor.MiddleCenter;
+            listing.Label("You will need to restart the game for any changes to take effect.");
+            GenUI.ResetLabelAlign();
+            GUI.color = savedColor;
+
+            listing.Gap();
+
+            Rect buttonsRect = listing.GetRect(30f);
+            Listing_Standard buttonsListing = new Listing_Standard();
+
+            buttonsListing.Begin(buttonsRect.LeftHalf());
+            if (buttonsListing.ButtonText("All On"))
+            {
+                settings.MJobs_Rescuing = true;
+                settings.MJobs_Operating = true;
+                settings.MJobs_Caring = true;
+                settings.MJobs_PriorityHauling = true;
+                settings.MJobs_PriorityCleaning = true;
+                settings.MJobs_Undertaking = true;
+                settings.MJobs_Recruiting = true;
+                settings.MJobs_AnimalTraining = true;
+                settings.MJobs_AnimalTaming = true;
+                settings.MJobs_Butchering = true;
+                settings.MJobs_Brewing = true;
+                settings.MJobs_Maintaining = true;
+                settings.MJobs_Deconstructing = true;
+                settings.MJobs_Harvesting = true;
+                settings.MJobs_Drilling = true;
+                settings.MJobs_Fabricating = true;
+                settings.MJobs_Synthesizing = true;
+                settings.MJobs_Refining = true;
+                settings.MJobs_Smelting = true;
+                settings.MJobs_Stonecutting = true;
+                settings.MJobs_Delivering = true;
+                settings.MJobs_Loading = true;
+                settings.MJobs_Scanning = true;
+            }
+            buttonsListing.End();
+
+            buttonsListing.Begin(buttonsRect.RightHalf());
+            if (buttonsListing.ButtonText("All Off"))
+            {
+                settings.MJobs_Rescuing = false;
+                settings.MJobs_Operating = false;
+                settings.MJobs_Caring = false;
+                settings.MJobs_PriorityHauling = false;
+                settings.MJobs_PriorityCleaning = false;
+                settings.MJobs_Undertaking = false;
+                settings.MJobs_Recruiting = false;
+                settings.MJobs_AnimalTraining = false;
+                settings.MJobs_AnimalTaming = false;
+                settings.MJobs_Butchering = false;
+                settings.MJobs_Brewing = false;
+                settings.MJobs_Maintaining = false;
+                settings.MJobs_Deconstructing = false;
+                settings.MJobs_Harvesting = false;
+                settings.MJobs_Drilling = false;
+                settings.MJobs_Fabricating = false;
+                settings.MJobs_Synthesizing = false;
+                settings.MJobs_Refining = false;
+                settings.MJobs_Smelting = false;
+                settings.MJobs_Stonecutting = false;
+                settings.MJobs_Delivering = false;
+                settings.MJobs_Loading = false;
+                settings.MJobs_Scanning = false;
+            }
+            buttonsListing.End();
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Rescuing.labelShort), ref settings.MJobs_Rescuing);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Rescuing.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Operating.labelShort), ref settings.MJobs_Operating);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Operating.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Caring.labelShort), ref settings.MJobs_Caring);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Caring.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_PriorityHauling.labelShort), ref settings.MJobs_PriorityHauling);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_PriorityHauling.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Undertaking.labelShort), ref settings.MJobs_Undertaking);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Undertaking.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Recruiting.labelShort), ref settings.MJobs_Recruiting);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Recruiting.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_AnimalTraining.labelShort), ref settings.MJobs_AnimalTraining);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_AnimalTraining.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_AnimalTaming.labelShort), ref settings.MJobs_AnimalTaming);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_AnimalTaming.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Butchering.labelShort), ref settings.MJobs_Butchering);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Butchering.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Brewing.labelShort), ref settings.MJobs_Brewing);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Brewing.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Maintaining.labelShort), ref settings.MJobs_Maintaining);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Maintaining.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Deconstructing.labelShort), ref settings.MJobs_Deconstructing);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Deconstructing.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Harvesting.labelShort), ref settings.MJobs_Harvesting);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Harvesting.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Drilling.labelShort), ref settings.MJobs_Drilling);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Drilling.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Fabricating.labelShort), ref settings.MJobs_Fabricating);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Fabricating.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Synthesizing.labelShort), ref settings.MJobs_Synthesizing);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Synthesizing.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Refining.labelShort), ref settings.MJobs_Refining);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Refining.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Smelting.labelShort), ref settings.MJobs_Smelting);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Smelting.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Stonecutting.labelShort), ref settings.MJobs_Stonecutting);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Stonecutting.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Delivering.labelShort), ref settings.MJobs_Delivering);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Delivering.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Loading.labelShort), ref settings.MJobs_Loading);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Loading.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listing.Gap();
+
+            listing.CheckboxLabeled(textInfo.ToTitleCase(WorkTypeDefOf.MJobs_Scanning.labelShort), ref settings.MJobs_Scanning);
+            Text.Font = GameFont.Tiny;
+            GUI.color = Color.gray;
+            listing.Label(WorkTypeDefOf.MJobs_Scanning.description);
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            listingRectCurrentHeight = listing.CurHeight;
+            listing.End();
+            Widgets.EndScrollView();
+            base.DoSettingsWindowContents(inRect);
+        }
+    }
+}
---
+++ Source/Assemblies/ManyJobsModSettings.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace ManyJobs
+{
+    public class ManyJobsModSettings : Verse.ModSettings
+    {
+        public bool MJobs_Rescuing = true;
+        public bool MJobs_Operating = true;
+        public bool MJobs_Caring = true;
+        public bool MJobs_PriorityHauling = true;
+        public bool MJobs_PriorityCleaning = true;
+        public bool MJobs_Undertaking = true;
+        public bool MJobs_Recruiting = true;
+        public bool MJobs_AnimalTraining = true;
+        public bool MJobs_AnimalTaming = true;
+        public bool MJobs_Butchering = true;
+        public bool MJobs_Brewing = true;
+        public bool MJobs_Maintaining = true;
+        public bool MJobs_Deconstructing = true;
+        public bool MJobs_Harvesting = true;
+        public bool MJobs_Drilling = true;
+        public bool MJobs_Fabricating = true;
+        public bool MJobs_Synthesizing = true;
+        public bool MJobs_Refining = true;
+        public bool MJobs_Smelting = true;
+        public bool MJobs_Stonecutting = true;
+        public bool MJobs_Delivering = true;
+        public bool MJobs_Loading = true;
+        public bool MJobs_Scanning = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref MJobs_Rescuing, "MJobs_Rescuing", true);
+            Scribe_Values.Look(ref MJobs_Operating, "MJobs_Operating", true);
+            Scribe_Values.Look(ref MJobs_Caring, "MJobs_Caring", true);
+            Scribe_Values.Look(ref MJobs_PriorityHauling, "MJobs_PriorityHauling", true);
+            Scribe_Values.Look(ref MJobs_PriorityCleaning, "MJobs_PriorityCleaning", true);
+            Scribe_Values.Look(ref MJobs_Undertaking, "MJobs_Undertaking", true);
+            Scribe_Values.Look(ref MJobs_Recruiting, "MJobs_Recruiting", true);
+            Scribe_Values.Look(ref MJobs_AnimalTraining, "MJobs_AnimalTraining", true);
+            Scribe_Values.Look(ref MJobs_AnimalTaming, "MJobs_AnimalTaming", true);
+            Scribe_Values.Look(ref MJobs_Butchering, "MJobs_Butchering", true);
+            Scribe_Values.Look(ref MJobs_Brewing, "MJobs_Brewing", true);
+            Scribe_Values.Look(ref MJobs_Maintaining, "MJobs_Maintaining", true);
+            Scribe_Values.Look(ref MJobs_Deconstructing, "MJobs_Deconstructing", true);
+            Scribe_Values.Look(ref MJobs_Harvesting, "MJobs_Harvesting", true);
+            Scribe_Values.Look(ref MJobs_Drilling, "MJobs_Drilling", true);
+            Scribe_Values.Look(ref MJobs_Fabricating, "MJobs_Fabricating", true);
+            Scribe_Values.Look(ref MJobs_Synthesizing, "MJobs_Synthesizing", true);
+            Scribe_Values.Look(ref MJobs_Refining, "MJobs_Refining", true);
+            Scribe_Values.Look(ref MJobs_Smelting, "MJobs_Smelting", true);
+            Scribe_Values.Look(ref MJobs_Stonecutting, "MJobs_Stonecutting", true);
+            Scribe_Values.Look(ref MJobs_Delivering, "MJobs_Delivering", true);
+            Scribe_Values.Look(ref MJobs_Loading, "MJobs_Loading", true);
+            Scribe_Values.Look(ref MJobs_Scanning, "MJobs_Scanning", true);
+
+            base.ExposeData();
+        }
+    }
+}
