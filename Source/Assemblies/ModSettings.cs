e07d71fa87370685b33810e8d00b28ef7352c355
Jeffery Harrell <jefferyharrell@gmail.com>
2023-09-18 12:13:23 -0700

    Clean up ModSettings.cs

--- Source/Assemblies/ModSettings.cs
+++ Source/Assemblies/ModSettings.cs
@@ -1,7 +1,6 @@
 using System;
 using RimWorld;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
@@ -40,20 +39,16 @@ namespace ManyJobs
         public bool MJobs_Loading;
         public bool MJobs_Scanning;
 
-        private readonly List<WorkType> WorkTypes = new List<WorkType>();
-        private readonly TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+        private readonly List<WorkType> _workTypes = new List<WorkType>();
 
-        private const string restartDialogMessage = "Changing the list of enabled work types requires a restart.\n\nRestart now? Unsaved progress will be lost.";
+        private const string RestartDialogMessage = "Changes to the list of enabled work types will not take effect until you restart the game.\n\nRestart now? Unsaved progress will be lost.";
 
-        private Listing_Standard workTypesListing = new Listing_Standard();
-        private Vector2 scrollPositionVector = Vector2.zero;
+        private readonly Listing_Standard _workTypesListing = new Listing_Standard();
+        private Vector2 _scrollPositionVector = Vector2.zero;
 
-        private const float buttonWidth = 100f;
-        private const float buttonHeight = GenUI.ListSpacing;
+        private float _maxWorkTypeNameWidth;
 
-        private float maxWorkTypeNameWidth;
-
-        private QuickSearchWidget _quickSearchWidget = new QuickSearchWidget();
+        private readonly QuickSearchWidget _quickSearchWidget = new QuickSearchWidget();
 
         private const string AllOnButtonLabel = "Enable All";
         private const string AllOffButtonLabel = "Disable All";
@@ -61,22 +56,24 @@ namespace ManyJobs
         private const string SelectedOffButtonLabel = "Disable Shown";
         private float _maximumButtonLabelSize;
 
+        private string _filter;
+        private bool _filtered;
+
         public ModSettings()
         {
             foreach (FieldInfo field in this.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance))
             {
-                WorkTypes.Add(new WorkType(field.Name));
+                _workTypes.Add(new WorkType(field.Name));
                 field.SetValue(this, true);
             }
         }
 
         public void OnLateInitialize()
         {
-            foreach (WorkType workType in WorkTypes)
+            foreach (WorkType workType in _workTypes)
             {
-                string n = textInfo.ToTitleCase(workType.Def?.labelShort ?? workType.Name);
-                Vector2 nSize = Text.CalcSize(n);
-                maxWorkTypeNameWidth = Mathf.Max(maxWorkTypeNameWidth, nSize.x);
+                Vector2 nameSize = Text.CalcSize(workType.LabelShort);
+                _maxWorkTypeNameWidth = Mathf.Max(_maxWorkTypeNameWidth, nameSize.x);
             }
 
             float allOnButtonLabelWidth = GenUI.Gap + Text.CalcSize(AllOnButtonLabel).x + GenUI.Gap;
@@ -94,7 +91,7 @@ namespace ManyJobs
         internal void WriteSettings()
         {
             bool isDirty = false;
-            foreach (WorkType workType in WorkTypes)
+            foreach (WorkType workType in _workTypes)
             {
                 if (workType.IsDirty)
                 {
@@ -105,7 +102,7 @@ namespace ManyJobs
 
             if (isDirty)
             {
-                Find.WindowStack.Add(new Dialog_MessageBox(restartDialogMessage, "Yes".Translate(), delegate
+                Find.WindowStack.Add(new Dialog_MessageBox(RestartDialogMessage, "Yes".Translate(), delegate
                 {
                     GenCommandLine.Restart();
                 }, "No".Translate(), null, null, true, null, null, WindowLayer.Dialog));
@@ -114,7 +111,7 @@ namespace ManyJobs
 
         public override void ExposeData()
         {
-            foreach (WorkType workType in WorkTypes)
+            foreach (WorkType workType in _workTypes)
             {
                 Scribe_Values.Look(ref workType.IsEnabled, workType.Name, true);
                 workType.IsEnabledInConfigFile = workType.IsEnabled;
@@ -124,30 +121,27 @@ namespace ManyJobs
             base.ExposeData();
         }
 
-        private string filter;
-        private bool filtered;
-
         public void DoSettingsWindowContents(Rect inRect)
         {
             string offButtonLabel = AllOffButtonLabel;
             string onButtonLabel = AllOnButtonLabel;
 
             List<WorkType> filteredWorkTypes;
-            if (filtered)
+            if (_filtered)
             {
-                filteredWorkTypes = WorkTypes.Where(wt => wt.Def.labelShort.Contains(filter) || wt.Def.description.Contains(filter)).ToList();
+                filteredWorkTypes = _workTypes.Where(wt => wt.Def.labelShort.Contains(_filter) || wt.Def.description.Contains(_filter)).ToList();
                 offButtonLabel = SelectedOffButtonLabel;
                 onButtonLabel = SelectedOnButtonLabel;
             }
             else
             {
-                filteredWorkTypes = WorkTypes;
+                filteredWorkTypes = _workTypes;
             }
 
             Rect offButtonRect = new Rect
             {
                 width = _maximumButtonLabelSize,
-                height = buttonHeight,
+                height = GenUI.ListSpacing,
             };
             offButtonRect.x = inRect.width - offButtonRect.width;
             offButtonRect.y = inRect.y;
@@ -156,36 +150,36 @@ namespace ManyJobs
             Rect onButtonRect = new Rect
             {
                 width = _maximumButtonLabelSize,
-                height = buttonHeight,
+                height = GenUI.ListSpacing,
             };
             onButtonRect.x = inRect.width - onButtonRect.width - GenUI.GapSmall - onButtonRect.width;
             onButtonRect.y = inRect.y;
             bool onButton = Widgets.ButtonText(onButtonRect, onButtonLabel);
 
-            Rect quickSearchRect = new Rect(inRect.x, inRect.y, inRect.width - _maximumButtonLabelSize - GenUI.GapSmall - _maximumButtonLabelSize - GenUI.GapSmall, buttonHeight);
+            Rect quickSearchRect = new Rect(inRect.x, inRect.y, inRect.width - _maximumButtonLabelSize - GenUI.GapSmall - _maximumButtonLabelSize - GenUI.GapSmall, GenUI.ListSpacing);
             _quickSearchWidget.OnGUI(quickSearchRect);
-            filter = _quickSearchWidget.filter.Text.ToLower();
-            filtered = !String.IsNullOrEmpty(filter);
+            _filter = _quickSearchWidget.filter.Text.ToLower();
+            _filtered = !String.IsNullOrEmpty(_filter);
 
             Rect outerRect = new Rect(inRect.x, inRect.y + quickSearchRect.height + GenUI.GapSmall, inRect.width, inRect.height - quickSearchRect.height - GenUI.GapSmall);
-            Rect innerRect = new Rect(inRect.x, inRect.y, inRect.width - (GenUI.ScrollBarWidth + GenUI.GapSmall), workTypesListing.CurHeight);
+            Rect innerRect = new Rect(inRect.x, inRect.y, inRect.width - (GenUI.ScrollBarWidth + GenUI.GapSmall), _workTypesListing.CurHeight);
             Rect listingRect = new Rect(innerRect.x, innerRect.y, innerRect.width, 99999f);
 
-            Widgets.BeginScrollView(outerRect, ref scrollPositionVector, innerRect, true);
-            workTypesListing.Begin(listingRect);
+            Widgets.BeginScrollView(outerRect, ref _scrollPositionVector, innerRect, true);
+            _workTypesListing.Begin(listingRect);
 
             TextAnchor originalAnchor = Text.Anchor;
             Text.Anchor = TextAnchor.MiddleLeft;
 
             foreach (WorkType workType in filteredWorkTypes)
             {
-                Rect rect = workTypesListing.GetRect(GenUI.GapTiny + Text.LineHeight + GenUI.GapTiny);
+                Rect rect = _workTypesListing.GetRect(GenUI.GapTiny + Text.LineHeight + GenUI.GapTiny);
 
-                Rect nameRect = new Rect(rect) { width = maxWorkTypeNameWidth + GenUI.Gap };
+                Rect nameRect = new Rect(rect) { width = _maxWorkTypeNameWidth + GenUI.Gap };
                 Rect descriptionRect = new Rect(rect) { x = nameRect.xMax, width = rect.width - nameRect.width - GenUI.Gap * 2 };
                 Rect clickableRect = new Rect(rect) { width = rect.width - Widgets.CheckboxSize };
 
-                Widgets.Label(nameRect, textInfo.ToTitleCase(workType.Def?.labelShort ?? workType.Name));
+                Widgets.Label(nameRect, workType.LabelShort);
 
                 GUI.color = Color.gray;
                 Widgets.Label(descriptionRect, workType.Def?.description ?? string.Empty);
@@ -213,7 +207,7 @@ namespace ManyJobs
 
             Text.Anchor = originalAnchor;
 
-            workTypesListing.End();
+            _workTypesListing.End();
             Widgets.EndScrollView();
 
             Rect versionInfoRect = new Rect(inRect.x, 0f, inRect.width, 40f);
